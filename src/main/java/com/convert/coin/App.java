c1ff1ee728c42a35949d41812480e9f6c1122ed0
Dgley Ramos <dgleysilva@gmail.com>
2023-01-20 14:16:25 -0300

    Iniciando conversor de moedas

---
+++ src/main/java/com/convert/coin/App.java
@@ -0,0 +1,13 @@
+package com.convert.coin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
