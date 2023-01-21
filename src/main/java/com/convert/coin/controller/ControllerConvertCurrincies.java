e7eed9bef7eb0a24f855e2a6729add6072e241f6
Dgley Ramos <dgleysilva@gmail.com>
2023-01-21 08:15:46 -0300

    Convert usd to brl

--- src/main/java/com/convert/coin/controller/ControllerConvertCurrincies.java
+++ src/main/java/com/convert/coin/controller/ControllerConvertCurrincies.java
@@ -9,12 +9,15 @@ public class ControllerConvertCurrincies {
     ServicecConvertCurrencies convert = new ServicecConvertCurrencies();
     
     public void ConvertCurrencies(double valor) throws Exception{
-        String opcao = JOptionPane.showInputDialog(null, "Escolha a moeda para a qual você deseja converter o seu dinheiro", "Moedas", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"De Reais para Dólares", "De Dóllar para Reias"}, "Escolha").toString();
+        String opcao = JOptionPane.showInputDialog(null, "Escolha a moeda para a qual você deseja converter o seu dinheiro", "Moedas", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"De Reais para Dólares", "De Dóllar para Reais"}, "Escolha").toString();
 
 
         switch(opcao){
             case "De Reais para Dólares":
                 convert.ConvertBrlToUsd(valor);
+                break;
+            case "De Dóllar para Reais":
+                convert.ConvertUsdToBrl(valor);
                 break;
         }
     }
--- src/main/java/com/convert/coin/service/ServicecConvertCurrencies.java
+++ src/main/java/com/convert/coin/service/ServicecConvertCurrencies.java
@@ -7,11 +7,17 @@ import com.convert.coin.model.Coin;
 public class ServicecConvertCurrencies {
     
     public void ConvertBrlToUsd(double valor) throws Exception{
-
         Coin brlToUsd = ServicePrice.PriceCoin("USD");
         double bid = brlToUsd.getBid();
         double valorMoeda = valor/bid;
         valorMoeda = (double) Math.round(valorMoeda * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem $ " + valorMoeda + " Dólares");
+    }
+    public void ConvertUsdToBrl(double valor) throws Exception{
+        Coin brlToUsd = ServicePrice.PriceCoin("USD");
+        double bid = brlToUsd.getBid();
+        double valorMoeda = valor * bid;
+        valorMoeda = (double) Math.round(valorMoeda * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
     }
 }
