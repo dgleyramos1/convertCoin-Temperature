77b6b5a0b270013f9e8187cea662495b32aebc07
Dgley Ramos <dgleysilva@gmail.com>
2023-01-21 08:32:52 -0300

    Convert ars to brl and brl to ars

--- src/main/java/com/convert/coin/controller/ControllerConvertCurrincies.java
+++ src/main/java/com/convert/coin/controller/ControllerConvertCurrincies.java
@@ -9,7 +9,19 @@ public class ControllerConvertCurrincies {
     ServicecConvertCurrencies convert = new ServicecConvertCurrencies();
     
     public void ConvertCurrencies(double valor) throws Exception{
-        String opcao = JOptionPane.showInputDialog(null, "Escolha a moeda para a qual você deseja converter o seu dinheiro", "Moedas", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"De Reais para Dólares", "De Dóllar para Reais"}, "Escolha").toString();
+        String opcao = JOptionPane.showInputDialog(
+            null,
+            "Escolha a moeda para a qual você deseja converter o seu dinheiro",
+            "Moedas",
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            new Object[]{
+                "De Reais para Dólares", 
+                "De Dóllar para Reais", 
+                "De Reais para Peso Argentino", 
+                "De Peso Argentino para Reais"
+            }, 
+            "Escolha").toString();
 
 
         switch(opcao){
@@ -18,6 +30,12 @@ public class ControllerConvertCurrincies {
                 break;
             case "De Dóllar para Reais":
                 convert.ConvertUsdToBrl(valor);
+                break;
+            case "De Reais para Peso Argentino":
+                convert.ConvertBrlToArs(valor);
+                break;
+            case "De Peso Argentino para Reais":
+                convert.ConvertArsToBrl(valor);
                 break;
         }
     }
--- src/main/java/com/convert/coin/service/ServicecConvertCurrencies.java
+++ src/main/java/com/convert/coin/service/ServicecConvertCurrencies.java
@@ -14,9 +14,23 @@ public class ServicecConvertCurrencies {
         JOptionPane.showMessageDialog(null, "Você tem $ " + valorMoeda + " Dólares");
     }
     public void ConvertUsdToBrl(double valor) throws Exception{
-        Coin brlToUsd = ServicePrice.PriceCoin("USD");
-        double bid = brlToUsd.getBid();
+        Coin usdToBrl = ServicePrice.PriceCoin("USD");
+        double bid = usdToBrl.getBid();
         double valorMoeda = valor * bid;
+        valorMoeda = (double) Math.round(valorMoeda * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
+    }
+    public void ConvertBrlToArs(double valor) throws Exception {
+        Coin brlToArs = ServicePrice.PriceCoin("brl-ars");
+        double bid = brlToArs.getBid();
+        double valorMoeda = valor*bid;
+        valorMoeda = (double) Math.round(valorMoeda * 100d) / 100;
+        JOptionPane.showMessageDialog(null, "Você tem $ " + valorMoeda + " Pesos Argentinos");
+    }
+    public void ConvertArsToBrl(double valor) throws Exception {
+        Coin arsToBrl = ServicePrice.PriceCoin("brl-ars");
+        double bid = arsToBrl.getBid();
+        double valorMoeda = valor/bid;
         valorMoeda = (double) Math.round(valorMoeda * 100d) / 100;
         JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
     }
