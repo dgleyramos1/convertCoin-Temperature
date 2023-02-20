f53d5bcf417af0bed18e188d83ca3e3a0aac67b4
Dgley ramos <dgleysilva@gmail.com>
2023-02-20 17:14:51 -0300

    Corrigindo erros de entradas de dados

--- src/main/java/com/convert/coin/App.java
+++ src/main/java/com/convert/coin/App.java
@@ -1,6 +1,5 @@
 package com.convert.coin;
 
-
 import javax.swing.JOptionPane;
 
 import com.convert.coin.controller.ControllerConvertCurrincies;
@@ -10,21 +9,28 @@ import com.convert.coin.controller.ControllerConvertTemperature;
  * Hello world!
  *
  */
-public class App{
+public class App {
 
     static ControllerConvertCurrincies controllerCoin = new ControllerConvertCurrincies();
     static ControllerConvertTemperature controllerTemperature = new ControllerConvertTemperature();
-    public static void main( String[] args ) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         int resposta = -1;
 
         do {
-            String opcoes = JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de Moeda", "Conversor de Temperatura"}, "Escolha" ).toString();
+            String opcoes = JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE,
+                    null, new Object[] { "Conversor de Moeda", "Conversor de Temperatura" }, "Escolha").toString();
 
-
-
-            switch(opcoes){
+            switch (opcoes) {
                 case "Conversor de Moeda":
                     String coin = JOptionPane.showInputDialog("Insira um valor");
+                    if (coin.matches("^[A-Za-z]*")) {
+                        resposta = JOptionPane.showConfirmDialog(null,
+                                "Por favor, coloque somente números! Deseja continuar?");
+                        finish(resposta);
+                        break;
+                    }
+                    System.out.println(coin);
                     double valor = Double.parseDouble(coin);
                     controllerCoin.ConvertCurrencies(valor);
                     resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
@@ -41,10 +47,10 @@ public class App{
         } while (resposta == 0);
     }
 
-    public static void finish(int resposta){
-        if(JOptionPane.OK_OPTION == resposta){
+    public static void finish(int resposta) {
+        if (JOptionPane.OK_OPTION == resposta) {
             System.out.println("Escolha opção Afirmativa");
-        } else{
+        } else {
             JOptionPane.showMessageDialog(null, "Progama finalizado");
         }
     }
--- src/main/java/com/convert/coin/service/ServiceConvertCurrencies.java
+++ src/main/java/com/convert/coin/service/ServiceConvertCurrencies.java
@@ -3,52 +3,62 @@ package com.convert.coin.service;
 import javax.swing.JOptionPane;
 
 public class ServiceConvertCurrencies {
-    public void ConvertBrlToUsd(double valor) throws Exception{
+    public void ConvertBrlToUsd(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-usd", false);
         JOptionPane.showMessageDialog(null, "Você tem $ " + valorMoeda + " Dólares");
     }
-    public void ConvertUsdToBrl(double valor) throws Exception{
+
+    public void ConvertUsdToBrl(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-usd", true);
         JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
     }
+
     public void ConvertBrlToArs(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-ars", false);
         JOptionPane.showMessageDialog(null, "Você tem $ " + valorMoeda + " Pesos Argentinos");
     }
+
     public void ConvertArsToBrl(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-ars", true);
         JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
     }
+
     public void ConvertBrlToEur(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-eur", false);
         JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Euros");
     }
+
     public void ConvertEurToBrl(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-eur", true);
         JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
     }
+
     public void ConvertBrlToGbp(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-gbp", false);
         JOptionPane.showMessageDialog(null, "Você tem $ " + valorMoeda + " Libras Estelinas");
     }
+
     public void ConvertGbpToBrl(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-gbp", true);
         JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
     }
+
     public void ConvertBrlToClp(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-clp", false);
         JOptionPane.showMessageDialog(null, "Você tem $ " + valorMoeda + " Libras Estelinas");
     }
+
     public void ConvertClpToBrl(double valor) throws Exception {
         double valorMoeda = Convert(valor, "brl-clp", true);
         JOptionPane.showMessageDialog(null, "Você tem R$ " + valorMoeda + " Reais");
     }
-    public double Convert(double valor, String moeda, boolean toBrl) throws Exception{
+
+    public double Convert(double valor, String moeda, boolean toBrl) throws Exception {
         double bid = ServicePrice.PriceCoin(moeda).getBid();
         double valorMoeda;
-        if(toBrl){
-            valorMoeda  = valor / bid;
-        }else{
+        if (toBrl) {
+            valorMoeda = valor / bid;
+        } else {
             valorMoeda = valor * bid;
         }
         return valorMoeda = (double) Math.round(valorMoeda * 100d) / 100;
